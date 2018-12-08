33516c0b179bbab0da2f2d822b8fb99cff87a1d4
eternal-flame-AD <eternal_flame-AD@protonmail.com>
2018-12-08 17:01:13 +0800

    add g++ dep

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir artifacts
 docker run -i --rm -v ${PWD}/artifacts:/root/artifacts golang:1.11-alpine sh << CMD
-apk add git gcc musl-dev
+apk add git gcc musl-dev g++
 cd /root
 git clone https://github.com/gohugoio/hugo.git
 cd hugo
